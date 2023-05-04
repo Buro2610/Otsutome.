5f2b304a095e8dbcfc4717d499d096eb3413fa9a
Onoda <you@example.com>
2023-05-04 15:33:37 +0900

    implement basic login 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,16 @@
 class SessionsController < ApplicationController
 
-
   def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
-        # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      reset_session      # ログインの直前に必ずこれを書くこと
+    if user && user.authenticate(params[:session][:password])
+      reset_session
       log_in user
       redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
     end
   end
@@ -21,8 +19,4 @@ class SessionsController < ApplicationController
     log_out
     redirect_to root_url, status: :see_other
   end
-
-
-
-
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -74,3 +74,4 @@ class LogoutTest < Logout
   end
 end
 
+
